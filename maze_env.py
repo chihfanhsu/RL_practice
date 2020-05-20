e844e0df6bfbde0ca4fc482b8c10b6a675c136cc
silver <hsuchihfan@gmail.com>
2020-05-19 21:22:18 -0700

    update code and policty evaluation

--- maze_env.py
+++ maze_env.py
@@ -15,13 +15,14 @@ class Maze(tk.Tk, object):
         super(Maze, self).__init__()
         self.action_space = ['0','1','2','3']
         self.title('Maze')
-        self.MAZE_Limit = np.array([6, 8]) # (Width,Height)
+        self.MAZE_Limit = np.array([4, 4]) # (Width,Height)
         self.geometry('{0}x{1}'.format(self.MAZE_Limit[0] * UNIT, self.MAZE_Limit[1] * UNIT))
         self.origin = np.array([0, 0]) # original loc of agent
-        self.cur_state = np.array([0, 0]) # agent loc
-        self.oval_state = np.array([4, 5]) # reward!
-        self.block_state = np.array([[1, 3], [2, 4]]) # you shall no pass!
-        self.hell_state =  np.array([4, 6]) # reward!
+        self.start_state =  np.array([1, 2])
+        self.cur_state = np.array([1, 2]) # agent loc
+        self.oval_state = np.array([[0, 0], [3, 3]]) # reward!
+        self.block_state = np.array([[2, 2]]) # you shall not pass!
+        self.hell_state =  np.array([[4, 4]]) # reward!
         self._build_maze()
     
     def _build_maze(self):
@@ -37,28 +38,30 @@ class Maze(tk.Tk, object):
             self.canvas.create_line(x0, y0, x1, y1)
 
         # set origin and oval (goal)
-        self.cur_state = self.origin.copy()
+        self.cur_state = self.start_state.copy()
         
         # draw environment
         origin_center = self.origin.copy() + int(.5*UNIT)
         
         #print(origin_center)
-        cur_center = origin_center
+        cur_center = origin_center + self.start_state.copy()*UNIT
         oval_center = origin_center + self.oval_state.copy()*UNIT
         block_center = origin_center + self.block_state.copy()*UNIT
         hell_center = origin_center + self.hell_state.copy()*UNIT
 
         # create goal
-        self.oval = self.canvas.create_oval(
-            oval_center[0] - 15, oval_center[1] - 15,
-            oval_center[0] + 15, oval_center[1] + 15,
-            fill='yellow')
-        
+        for i in range(len(self.oval_state)):
+            self.oval = self.canvas.create_oval(
+                oval_center[i,0] - 15, oval_center[i,1] - 15,
+                oval_center[i,0] + 15, oval_center[i,1] + 15,
+                fill='yellow')
+
         # create hell
-        self.hell = self.canvas.create_oval(
-            hell_center[0] - 15, hell_center[1] - 15,
-            hell_center[0] + 15, hell_center[1] + 15,
-            fill='red')
+        for i in range(len(self.hell_state)):
+            self.hell = self.canvas.create_oval(
+                hell_center[i,0] - 15, hell_center[i,1] - 15,
+                hell_center[i,0] + 15, hell_center[i,1] + 15,
+                fill='red')
 
         # create agent
         self.rect = self.canvas.create_rectangle(
@@ -80,24 +83,26 @@ class Maze(tk.Tk, object):
         self.canvas.delete(self.rect)
         self.canvas.delete(self.oval)
         self.canvas.delete(self.hell)
-        self.cur_state = self.origin.copy()
+        self.cur_state = self.start_state.copy()
         
         origin_center = self.origin.copy() + int(.5*UNIT)
-        cur_center = origin_center
+        cur_center = origin_center + self.start_state.copy()*UNIT
         oval_center = origin_center + self.oval_state.copy()*UNIT
         hell_center = origin_center + self.hell_state.copy()*UNIT
         
         # create oval
-        self.oval = self.canvas.create_oval(
-            oval_center[0] - 15, oval_center[1] - 15,
-            oval_center[0] + 15, oval_center[1] + 15,
-            fill='yellow')
+        for i in range(len(self.oval_state)):
+            self.oval = self.canvas.create_oval(
+                oval_center[i,0] - 15, oval_center[i,1] - 15,
+                oval_center[i,0] + 15, oval_center[i,1] + 15,
+                fill='yellow')
         
         # create hell
-        self.hell = self.canvas.create_oval(
-            hell_center[0] - 15, hell_center[1] - 15,
-            hell_center[0] + 15, hell_center[1] + 15,
-            fill='red')
+        for i in range(len(self.hell_state)):
+            self.hell = self.canvas.create_oval(
+                hell_center[i,0] - 15, hell_center[i,1] - 15,
+                hell_center[i,0] + 15, hell_center[i,1] + 15,
+                fill='red')
 
         # create red rect
         self.rect = self.canvas.create_rectangle(
@@ -107,7 +112,7 @@ class Maze(tk.Tk, object):
         
         return True
 
-    def take_action(self, action):
+    def take_action(self, action, show_animate = False):
         next_state = self.cur_state.copy()
         invalid_move = False
         
@@ -139,20 +144,59 @@ class Maze(tk.Tk, object):
             
         # draw animation
         next_center = (next_state-self.cur_state)*UNIT
-                
-        self.canvas.move(self.rect, next_center[0], next_center[1])  # move agent
+        
+        if show_animate:
+            self.canvas.move(self.rect, next_center[0], next_center[1])  # move agent
+        
         self.cur_state = next_state.copy()
        
-        if ((self.cur_state ==self.oval_state).all()):
-            print ("you got it !!!")
-            self.reset()
+        if np.any(np.all(self.cur_state == self.oval_state, axis=1)):
+            next_state = self.cur_state.copy()
+            return True
             
-        if ((self.cur_state ==self.hell_state).all()):
-            print ("you die !!!")
-            self.reset()
+        if np.any(np.all(self.cur_state == self.hell_state, axis=1)):
+            next_state = self.cur_state.copy()
+            return True
                 
-        return True
+        return False
 
     def render(self):
         time.sleep(0.001)
         self.update()
+        
+    def position2state(self, pos, inv = False):
+        if inv ==False:
+            in_pos = np.array(pos)
+            if in_pos.shape == (2,):
+                return in_pos[0]*self.MAZE_Limit[1] + in_pos[1]
+            else:
+                print("Error corrdinate!")
+                return False
+        else:
+            in_pos = np.array(pos)
+            if in_pos.shape == ():
+                return np.array([np.int(np.floor(in_pos/self.MAZE_Limit[1])), np.int(in_pos%self.MAZE_Limit[1])])
+            else:
+                print("Error state number")
+                return False
+            
+    def get_env_model(self):
+        # def gen_p_matrix(env):
+        p = np.zeros((len(self.action_space),self.MAZE_Limit[0]*self.MAZE_Limit[1],self.MAZE_Limit[0]*self.MAZE_Limit[1]))
+        for i in range(self.MAZE_Limit[0]):
+            for j in range(self.MAZE_Limit[1]):
+                for a in range(len(self.action_space)):
+                    self.cur_state = np.array([i,j]).copy()
+                    self.take_action(a,show_animate=False)
+                    p[a, self.position2state([i,j]), self.position2state(self.cur_state)] = 1
+                    
+        #set P matrix of oval state
+        for a in range(len(self.action_space)):
+            for t in range(len(self.oval_state)):
+                p[a,self.position2state(self.oval_state[t])] = 0
+                
+        #set P matrix of oval state
+        for a in range(len(self.action_space)):
+            for t in range(len(self.block_state)):
+                p[a,self.position2state(self.block_state[t])] = 0        
+        return p
