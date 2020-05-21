974a33bc8453d17f61adbb5ca99ef54d25f33f77
helen.chang <helen.chang@wemoscooter.com>
2020-05-21 15:18:41 -0700

    VALUE ITERATIOIN. (Ignore block).

--- policy_eval_helen.py
+++ policy_eval_helen.py
@@ -19,55 +19,24 @@ env[termin] = 0
 # block = tuple(map(tuple, np.transpose(maz.block_state)))
 # env[block] = 0
 
-# reward
-r = -1
-p0 = np.tile(r, s) * env
 
-# action
-# up
-p1 = p0.copy()
-# down
-p2 = p0.copy()
-# right
-p3 = p0.copy()
-# left
-p4 = p0.copy()
-
-# initialize value
-value = np.zeros(s)
 for i in range(10):
+    # reward
+    r = -1
+    p0 = np.tile(r, s) * env
     if i==0:
+        # initialize value
+        value = np.zeros(s)
         print(i)
         print(value)
-        # define next state (if outside of box (wall) -> back to previous state)
-        b = p1[2+1, 2] # keep the state where next state will be block.
-        p1 = p1[(0, 0, 1, 2), :] * env # up
-        p1[2+1, 2] = b
+    # define next state (if outside of box (wall) -> back to previous state)
+    p1 = value[(0, 0, 1, 2), :] * env # up
+    p2 = value[(1, 2, 3, 3), :] * env # down
+    p3 = value[:, (0, 0, 1, 2)] * env # left
+    p4 = value[:, (1, 2, 3, 3)] * env # right
+
+    next_state = np.amax(np.concatenate([p1[np.newaxis], p2[np.newaxis], p3[np.newaxis], p4[np.newaxis]]), 0)
+    policy = np.argmax(np.concatenate([p1[np.newaxis], p2[np.newaxis], p3[np.newaxis], p4[np.newaxis]]), 0)
         
-        b = p2[2-1, 2] # keep the state where next state will be block.
-        p2 = p2[(1, 2, 3, 3), :] * env # down
-        p2[2-1, 2] = b
-        
-        b = p3[2, 2+1] # keep the state where next state will be block.
-        p3 = p3[:, (0, 0, 1, 2)] * env # left
-        p3[2, 2+1] = b
-        
-        b = p4[2, 2-1] # keep the state where next state will be block.
-        p4 = p4[:, (1, 2, 3, 3)] * env # right
-        p4[2, 2-1] = b
-    else:
-        # define next state (if outside of box (wall) -> back to previous state)
-        p1 = value[(0, 0, 1, 2), :] * env # up
-        p1[2+1, 2] = value[2+1, 2] # keep the state where next state will be block.
-        
-        p2 = value[(1, 2, 3, 3), :] * env # down
-        p2[2-1, 2] = value[2-1, 2] # keep the state where next state will be block.
-        
-        p3 = value[:, (0, 0, 1, 2)] * env # left
-        p3[2, 2+1] = value[2, 2+1] # keep the state where next state will be block.
-        
-        p4 = value[:, (1, 2, 3, 3)] * env # right
-        p4[2, 2-1] = value[2, 2-1] # keep the state where next state will be block.
-        
-    value = p0 + 0.25 * (p1+p2+p3+p4)
-    print(i+1, value)
+    value = p0 + next_state
+    print(i+1, policy)
